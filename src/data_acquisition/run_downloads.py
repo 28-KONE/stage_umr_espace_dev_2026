ec7714c398003fa8d36362cdde97759c07904044
KONE  Damba <80094969+28-KONE@users.noreply.github.com>
2026-09-10 16:28:58 +0200

    Refactor run_downloads.py for improved clarity

--- src/data_acquisition/run_downloads.py
+++ src/data_acquisition/run_downloads.py
@@ -3,24 +3,16 @@ import sys
 
 def run_script(script_name):
     print(f"\n=== Lancement de {script_name} ===\n")
-
-    result = subprocess.run(
-        [sys.executable, script_name],
-        check=True
-    )
-
-    print(f"\n=== {script_name} terminé avec succès ✅ ===\n")
-
+    result = subprocess.run([sys.executable, script_name], check=True)
+    print(f"\n=== {script_name} terminé avec succès ! ===\n")
 
 def main():
     try:
         run_script("download_sentinel-1.py")
         run_script("download_sentinel-2.py")
-
-        print("\n✅ Tous les téléchargements sont terminés sans erreur")
+        print("\n Tous les téléchargements sont terminés sans erreur")
     except subprocess.CalledProcessError as e:
-        print("\n❌ Erreur pendant l'exécution :", e)
-
+        print("\n Erreur pendant l'exécution :", e)
 
 if __name__ == "__main__":
     main()
